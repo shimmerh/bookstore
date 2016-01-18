f9fd33554a9cc958a8331f27e6ca6a0bcab42f4c
shimmerh <i@shimmerh.com>
2016-01-18 10:02:29 +0800

    first commit

---
+++ websocket/migrations/0002_auto_20160117_0024.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('websocket', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('title', models.CharField(max_length=3, choices=[(b'MR', b'Mr.'), (b'MRS', b'Mrs'), (b'MR', b'Ms.')])),
+                ('birth_date', models.DateField(null=True, blank=True)),
+                ('email', models.EmailField(max_length=254)),
+                ('headshot', models.ImageField(upload_to=b'author_headshot')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('publication_date', models.DateField()),
+                ('authors', models.ManyToManyField(to='websocket.Author')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Publisher',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30)),
+                ('address', models.CharField(max_length=50)),
+                ('city', models.CharField(max_length=60)),
+                ('state_province', models.CharField(max_length=30)),
+                ('country', models.CharField(max_length=50)),
+                ('website', models.URLField()),
+            ],
+            options={
+                'ordering': ['-name'],
+            },
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='publisher',
+            field=models.ForeignKey(to='websocket.Publisher'),
+        ),
+    ]
